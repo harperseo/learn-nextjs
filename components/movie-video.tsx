5ea4bbe930b1599af0fe9c240f1f97152844b624
Harper <eunbyul1866@gmail.com>
2024-05-10 11:52:07 +0900

    test commit

--- components/movie-video.tsx
+++ components/movie-video.tsx
@@ -1,8 +1,8 @@
 import { URL } from "../app/(home)/page";
 
 async function getVideo(id: string) {
-  console.log(`Fetching video: ${Date.now()}`);
   await new Promise((resolve) => setTimeout(resolve, 500));
+  console.log(`Fetching video: ${Date.now()}`);
   const res = await fetch(`${URL}/${id}/videos`);
   return res.json();
 }
